a0f5a37cc20895ac855955d249936b4b3bb4a58c
Lumi <kallioniemi@pm.me>
2022-06-06 00:46:11 +0300

    Solve Lecture 1 exercises

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -39,6 +39,7 @@ module Lecture1
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+makeSnippet :: Int -> [Char] -> [Char]
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
@@ -53,8 +54,10 @@ their squares.
 Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
+
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: Int -> Int -> Int
+sumOfSquares x y = (x ^ (2 :: Integer)) + (y ^ (2 :: Integer))
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -67,7 +70,8 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = mod (abs n) 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -81,7 +85,12 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+
+minmax :: Int -> Int -> Int -> Int
+minmax x y z = maxNumber - minNumber
+  where
+    maxNumber = foldl max x [x, y, z]
+    minNumber = foldl min x [x, y, z]
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -98,7 +107,32 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+
+subString :: Int -> Int -> [Char] -> [Char]
+subString start end str =
+  take amountToTake (drop amountToDrop str)
+    where
+      amountToDrop = max 0 start
+      amountToTake = (if end < 0 then -1 else end - amountToDrop) + 1
+
+-- subString :: Int -> Int -> [Char] -> [Char]
+-- subString start end str =
+--   let
+--     actualStart = max 0 start
+--   in
+--   if end >= actualStart && end >= 0
+--     then 
+--       take (end - actualStart + 1) (drop actualStart str)
+--     else 
+--       ""
+
+-- subString start end str = foldl goFold "" (zip [0..] str)
+--   where
+--     goFold :: [Char] -> (Int, Char) -> [Char]
+--     goFold result (currentIndex, currentChar) =
+--       if currentIndex >= start && currentIndex <= end
+--         then result ++ [currentChar]
+--         else result
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -108,7 +142,12 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
+
+strSum :: String -> Int
+strSum str = foldl (+) 0 (map toInt (words str))
+  where
+    toInt :: String -> Int
+    toInt item = read item :: Int
 
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
@@ -123,4 +162,31 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+
+lowerAndGreater :: Int -> [Int] -> String
+lowerAndGreater n list = show n ++ " is greater than " ++ show amountNGreaterThan ++ " elements and lower than " ++ show amountNLowerThan ++ " elements"
+  where
+    fold :: (Int, Int) -> Int -> (Int, Int)
+    fold (greaterThan, lowerThan) current = 
+      if current < n 
+        then (greaterThan + 1, lowerThan) 
+        else
+          if current > n 
+            then (greaterThan, lowerThan + 1)
+            else (greaterThan, lowerThan)
+    (amountNGreaterThan, amountNLowerThan) = foldl fold (0, 0) list
+
+-- lowerAndGreater :: Int -> [Int] -> String
+-- lowerAndGreater n list = show n ++ " is greater than " ++ show amountNGreaterThan ++ " elements and lower than " ++ show amountNLowerThan ++ " elements"
+--   where
+--     go :: (Int -> Bool) -> Int -> [Int] -> Int
+--     go compareFn result remainingList = 
+--       if null remainingList
+--         then result
+--         else
+--           if compareFn (head remainingList)
+--             then go compareFn (result + 1) (tail remainingList)
+--             else go compareFn result (tail remainingList)
+--     amountNGreaterThan = go (n >) 0 list
+--     amountNLowerThan = go (n <) 0 list
+
