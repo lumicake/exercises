c55cb92cd93da406815b2af069ddd9048b9d9a64
Lumi <kallioniemi@pm.me>
2022-06-05 18:36:25 +0300

    Set up direnv

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Haskell exercises";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          bashInteractive
+          haskell.compiler.ghc8107
+          cabal-install
+        ];
+        buildInputs = [ ];
+      };
+    });
+}
+
